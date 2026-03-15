ec05e5584250b3e1d7ebbd4b29346072f874e7e2
danila <danilaeremenko@mail.ru>
2026-03-15 23:24:03 +0300

    confs updated

--- TALENT/model/utils.py
+++ TALENT/model/utils.py
@@ -397,6 +397,7 @@ def get_deep_args():
                             'nwck_wd_nn_neurons_cl_params_b_params_re_init_max_no',
 
                             'nwck_wd_nn_neurons_lin_nn_cl_params_b_params',
+                            'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_fmask_no',
                             'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re',
                             'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re_cdist',
                             'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re_rbp',
@@ -969,6 +970,7 @@ def get_method(model):
         'nwck_wd_nn_neurons_cl_params_b_params_re_init_max_no',
 
         'nwck_wd_nn_neurons_lin_nn_cl_params_b_params',
+        'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_fmask_no',
         'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re',
         'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re_cdist',
         'nwck_wd_nn_neurons_lin_nn_cl_params_b_params_re_rbp',
