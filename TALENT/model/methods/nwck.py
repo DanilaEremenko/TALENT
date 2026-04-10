3ee74d5c2ef3c81313b7a4680e4b822abf1a215c
danila <danilaeremenko@mail.ru>
2026-04-10 20:43:34 +0300

    eval_stats saving added

--- TALENT/model/methods/nwck.py
+++ TALENT/model/methods/nwck.py
@@ -475,7 +475,7 @@ class NWCKMethod(Method):
                             cl_T_probs=cl_T_probs.detach().cpu().numpy().tolist(),
                             cl_B_probs=cl_B_probs.detach().cpu().numpy().tolist(),
                             y_pred=y_pred.detach().cpu().numpy().tolist(),
-                            y_preds_indep=y_preds_indep.detach().cpu().numpy().tolist()
+                            y_preds_indep=y_preds_indep.detach().cpu().numpy().tolist() if y_pred_indep is not None else None
                         )
                     )
                 test_logit.append(y_pred.squeeze(-1))
