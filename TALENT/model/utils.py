6b38a6b02ee8aad47743bde9c51e434b26d51323
danila <danilaeremenko@mail.ru>
2026-04-12 04:20:34 +0300

    hard confs added

--- TALENT/model/utils.py
+++ TALENT/model/utils.py
@@ -411,7 +411,7 @@ def get_deep_args():
 
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_clgum',
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh',
-                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh_clgum'
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh_clgum',
 
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh_clgum',
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh',
@@ -998,7 +998,7 @@ def get_method(model):
 
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_clgum',
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh',
-        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh_clgum'
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_evh_clgum',
 
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh_clgum',
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh',
