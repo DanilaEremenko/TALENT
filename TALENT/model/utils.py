d6d6de446a94af0548d641af6a9d16dcc114746f
danila <danilaeremenko@mail.ru>
2026-04-13 00:58:32 +0300

    confs updated

--- TALENT/model/utils.py
+++ TALENT/model/utils.py
@@ -421,6 +421,21 @@ def get_deep_args():
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_evh_clgum',
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_clgum',
 
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_clgum',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_re_clgum',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau',
+
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_cl_tau',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_cl_tau_re_clgum',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_rcl_cl_tau',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_rcl_cl_tau_re_clgum',
+
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_cl_tau',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_cl_tau_re_clgum',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_rcl_cl_tau',
+                            'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_rcl_cl_tau_re_clgum',
+
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh_clgum',
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh',
                             'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_clgum',
@@ -1015,6 +1030,21 @@ def get_method(model):
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_evh',
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_evh_clgum',
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_clgum',
+
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau_re_clgum',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_cl_tau',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau_re_clgum',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifs_rcl_cl_tau',
+
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_cl_tau',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_cl_tau_re_clgum',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_rcl_cl_tau',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_rcl_cl_tau_re_clgum',
+
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_cl_tau',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_cl_tau_re_clgum',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_rcl_cl_tau',
+        'nwck_wd_rerere_fmask_no_act_fn_relu_ift_pen_indep_mean_rcl_cl_tau_re_clgum',
 
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh_clgum',
         'nwck_wd_rerere_fmask_no_act_fn_relu_ifo_pen_indep_mean_re_nocwd2_cl_tau_evh',
