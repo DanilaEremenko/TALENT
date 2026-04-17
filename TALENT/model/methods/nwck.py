e4d32234a683305620a9782b61678e6612d6a5ee
danila <danilaeremenko@mail.ru>
2026-04-17 19:36:53 +0300

    rcl params updated

--- TALENT/model/methods/nwck.py
+++ TALENT/model/methods/nwck.py
@@ -119,12 +119,13 @@ class NWCKMethod(Method):
         }
         model_config['clust_model_params']['clust_model_fspace_weight_decay'] = 0
 
-        if 'clust_model_weight_decay' not in model_config['clust_model_params']:
-            if 'clust_model_lr1' in model_config['clust_model_params'].keys():
-                model_config['clust_model_params']['clust_model_weight_decay1'] = 0.0
-                model_config['clust_model_params']['clust_model_weight_decay2'] = 0.0
-            else:
-                model_config['clust_model_params']['clust_model_weight_decay'] = 0.0
+        for key in ['clust_model_weight_decay', 'clust_model_scale_weight_decay']:
+            if key not in model_config['clust_model_params']:
+                if 'clust_model_lr1' in model_config['clust_model_params'].keys():
+                    model_config['clust_model_params'][f'{key}1'] = 0.0
+                    model_config['clust_model_params'][f'{key}2'] = 0.0
+                else:
+                    model_config['clust_model_params'][key] = 0.0
 
         meta_common_params = {
             'nn_background_lr': None,
