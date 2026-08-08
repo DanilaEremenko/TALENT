ade081a2377ae1b6ec3c59cc8edadbf33e8db4b1
danila <danilaeremenko@mail.ru>
2026-08-09 00:07:36 +0300

    logreg eval stats added

--- TALENT/model/classical_methods/logreg.py
+++ TALENT/model/classical_methods/logreg.py
@@ -38,4 +38,7 @@ class LogRegMethod(classical_methods):
         test_label = self.y_test
         test_logit = self.model.predict_proba(self.N_test)
         vres, metric_name = self.metric(test_logit, test_label, self.y_info)
+        self.eval_stats = dict(
+            coef=self.model.coef_,
+        ) if do_eval_stats else None
         return vres, metric_name, test_logit
